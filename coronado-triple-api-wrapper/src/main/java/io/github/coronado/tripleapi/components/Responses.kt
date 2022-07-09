d705890232b23a487f3cceb9baaf167eeb679384
Phillip Ross (Numo OSS) <phillip.ross@numo.com>
2022-07-08 22:16:59 -0400

    - documentation updates for triple api components
    Signed-off-by: Phillip Ross (Numo OSS) <phillip.ross@numo.com>

--- coronado-triple-api-wrapper/src/main/java/io/github/coronado/tripleapi/components/Responses.kt
+++ coronado-triple-api-wrapper/src/main/java/io/github/coronado/tripleapi/components/Responses.kt
@@ -5,6 +5,10 @@ import io.github.coronado.tripleapi.components.schemas.EntityId
 import io.github.coronado.tripleapi.components.schemas.ExternalId
 import io.github.coronado.tripleapi.components.schemas.Publisher
 
+/**
+ * A Kotlin implementation for the response components established by Triple API Swagger v1.1 (as of 2022-06-27)
+ */
+
 data class PublishersResponse(val publishers: List<PublisherResponsePublisherItem>)
 data class PublisherResponsePublisherItem(val id: EntityId, @Json(name = "external_id") val externalId: ExternalId, @Json(name = "assumed_name") val assumedName: String)
 typealias PublisherResponse = Publisher
---
+++ coronado-triple-api-wrapper/src/main/java/io/github/coronado/tripleapi/components/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Kotlin implementation for the OpenAPI components established by Triple API Swagger v1.1 (as of 2022-06-27)
+ */
+package io.github.coronado.tripleapi.components;
